44eda351675e858ede7721c069a659c1d75b90a1
BautistaFernandez <141528279+BautistaFernandez@users.noreply.github.com>
2026-04-19 16:40:34 -0300

    demondoll kinematic

--- Assets/Scripts/Doll/DemonDoll.cs
+++ Assets/Scripts/Doll/DemonDoll.cs
@@ -1,17 +1,40 @@
 using UnityEngine;
 
 public class DemonDoll : MonoBehaviour
-{ 
+{
+    /*private Rigidbody rb;
+
+    public void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }*/
+
     public void Aparision()
     {
         gameObject.SetActive(true);
+
+        /*if (rb != null )
+        {
+            rb.isKinematic = false;
+        }*/
         // GetComponent<AudioSource>().Play();
         // GetComponent<Animator>().SetTrigger("Susto");
-        Debug.Log("La muñeca dice: ¡Aquí estoy!");
+        Debug.Log("Evento: Aparece la doll");
+
+
     }
 
     public void Invisible()
     {
         gameObject.SetActive(false);
     }
+
+    /*public void GravityActive()
+    {
+        if (rb != null)
+        {
+            rb.isKinematic = ;
+        }
+    }*/
 }
+
