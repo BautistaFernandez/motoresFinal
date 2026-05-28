48d4645e9b42b82391bda39e2e898a055f068817
BillieJoe <mauricio.arbelaiz@gmail.com>
2026-05-28 15:51:46 -0300

    ADD, ShadowCube, unido puzzle caja fusibles

--- Assets/Scripts/Game/CajaDeLuz.cs
+++ Assets/Scripts/Game/CajaDeLuz.cs
@@ -3,33 +3,43 @@ using UnityEngine;
 public class CajaDeLuz : MonoBehaviour
 {
     [Header("Configuración de Luces")]
-    [Tooltip("Arrastrá acá las luces individuales del garage (Focus_01, Focus_01.002, etc.)")]
     [SerializeField] private GameObject[] lucesGarage;
 
-    
+    [Header("Obstáculo de Oscuridad")]
+    [Tooltip("Arrastrá acá el Cubo Negro que bloquea el paso")]
+    [SerializeField] private GameObject cuboBloqueador;
+
     public bool YaSeActivo { get; private set; } = false;
 
     void Start()
     {
-        if (lucesGarage != null)
+     
+        if (cuboBloqueador != null)
         {
-            ApagarLuces();
+            cuboBloqueador.SetActive(true);
         }
+        ApagarLuces();
     }
 
     public void EncenderEnergia()
     {
         if (YaSeActivo) return;
-
         YaSeActivo = true;
 
+       
+        if (cuboBloqueador != null)
+        {
+            Destroy(cuboBloqueador);
+        }
+
+        // Encendemos tus focos reales del techo
         if (lucesGarage != null && lucesGarage.Length > 0)
         {
             foreach (GameObject luz in lucesGarage)
             {
                 if (luz != null) luz.SetActive(true);
             }
-            Debug.Log("¡Energía restablecida!");
+            Debug.Log("¡Fusible colocado! Cubo eliminado y garage abierto.");
         }
     }
 
